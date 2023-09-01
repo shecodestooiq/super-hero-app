fc68795db45c19ea8ab4e48145f0c609bc43ee8c
sanguine-dreams <sara0dhahir@gmail.com>
2023-09-01 16:22:43 +0300

    initial requirment complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 function App() {
-  return <>Super Hero App</>;
+  const [heroes, setHeroes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://superheroapi.com/api/257605103806175/1")
+      .then((response) => {
+        setHeroes(response.data);
+      });
+  }, []);
+  
+  return (
+    <>
+      <h1>{heroes.name}</h1>
+      <img src={heroes.image.url} />
+    </>
+  );
 }
 
 export default App;
